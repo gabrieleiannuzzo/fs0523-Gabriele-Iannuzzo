8b46db29b8a880e88eb4c1576d045d182b5f3c60
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2023-10-13 12:39:28 +0200

    primo commit progetto settimanale settimana 3

--- m1/s3/g4/js/script.js
+++ m1/s3/g4/js/script.js
@@ -1,85 +1,99 @@
-let tombola = document.querySelector(".estrazione");
+generaTombola();
+generaCartelle();
+funzionamento();
 
-for (let i = 1; i <= 76; i++) {
-    let numero = document.createElement("div");
-    let p = document.createElement("p");
-    let check = document.createElement("div");
-    p.innerText = i;
-    numero.classList.add("numero");
-    if (i % 5 == 0 && i % 10 != 0) numero.classList.add("multiplo5");
-    tombola.append(numero);
-    numero.append(p);
-    numero.append(check);
-}
+// FUNZIONE PER GENERARE LA TOMBOLA
+function generaTombola() {
+    let tombola = document.querySelector(".estrazione");
+    
+    for (let i = 1; i <= 76; i++) {
+        let numero = document.createElement("div");
+        let p = document.createElement("p");
+        let check = document.createElement("div");
+        p.innerText = i;
+        numero.classList.add("numero");
+        if (i % 5 == 0 && i % 10 != 0) numero.classList.add("multiplo5");
+        tombola.append(numero);
+        numero.append(p);
+        numero.append(check);
+    }
+};
 
-let input = document.querySelector(".inputs input");
-let firstButton = document.getElementById("start");
-let target = document.querySelector(".giocatore");
+// FUNZIONE PER GENERARE LE CARTELLE
+function generaCartelle() {
+    let input = document.querySelector(".inputs input");
+    let firstButton = document.getElementById("start");
+    let target = document.querySelector(".giocatore");
 
-firstButton.addEventListener("click", () => {
-    if (input.value > 0 && input.value <= 5) {
-        for (let i = 1; i <= input.value; i++) {
-            let cartella = document.createElement("div");
-            let primaFascia = document.createElement("div");
-            let caselle = [];
-
-            cartella.classList.add("cartella")
-            primaFascia.innerText = `Cartella ${i}`;
-            primaFascia.classList.add("div1");
-
-            target.append(cartella);
-            cartella.append(primaFascia);
-
-            for (let j = 0; j < 3; j++) {
-                let secondaFascia = document.createElement("div");
-                secondaFascia.classList.add("div2");
+    firstButton.addEventListener("click", () => {
+        if (input.value > 0 && input.value <= 5) {
+            for (let i = 1; i <= input.value; i++) {
+                let cartella = document.createElement("div");
+                let primaFascia = document.createElement("div");
                 
-                for (let k = 0; k < 8; k++) {
-                    let casella = document.createElement("div");
-                    secondaFascia.append(casella);
-                    caselle.push(casella);
-                    casella.innerText = ((k + 1) + (8 * j));
-                }
+                cartella.classList.add("cartella")
+                primaFascia.innerText = `Cartella ${i}`;
+                primaFascia.classList.add("div1");
+                
+                target.append(cartella);
+                cartella.append(primaFascia);
+                
+                let array = [];
+                
+                for (let j = 0; j < 3; j++) {
+                    let secondaFascia = document.createElement("div");
+                    secondaFascia.classList.add("div2");
+                    
+                    for (let k = 0; k < 8; k++) {
+                        let casella = document.createElement("div");
+                        secondaFascia.append(casella);
 
-                cartella.append(secondaFascia);
+                        casella.innerText = random(array);
+                    }
+                    
+                    cartella.append(secondaFascia);
+                }
             }
         }
+        
+        input.value = '';
+    });
+}
+
+// FUNZIONE PER GENERARE RANDOMICAMENTE I NUMERI DELLE CARTELLE E DELLA TOMBOLA
+function random(arr) {
+    let numeroRandom = Math.floor(Math.random() * 76) + 1;
+    if (!arr.includes(numeroRandom)) {
+        arr.push(numeroRandom);
+        return numeroRandom;
     }
 
-    input.value = '';
-});
+    return random(arr);
+}
 
-let random = document.getElementById("random");
-let numeri = [];
-for (let n = 1; n <= 76; n++) numeri[n] = n;
+// FUNZIONE CHE GESTISCE IL FUNZIONAMENTO DEL TABELLONE E DELLE CARTELLE ALL'ESTRAZIONE DI UN NUMERO
+function funzionamento () {
+    let numeriTombola = [];
+    let button = document.getElementById("random");
 
-random.addEventListener("click", () => {
-    let numeroEstratto = estrazione(numeri);
-    let cartelle = document.querySelectorAll(".div2");
-    let numero = document.querySelectorAll(".numero");
-    let nuovoNumero = document.querySelector(".cartelle h2");
-
-    nuovoNumero.innerText = `Ultimo numero estratto: ${estrazione}`;
-
-    for (let n of numero) {
-        let div = n.querySelector("div");
-        if (n.innerText == numeroEstratto) div.classList.add("check");
-
-    }
-
-    for (let cartella of cartelle) {
-        let caselle = document.querySelectorAll("div");
-        for (casella of caselle) {
-            if (casella.innerText == numeroEstratto) casella.classList.add("estratto");
+    button.addEventListener("click", () => {
+        let numeroRandom = random(numeriTombola);
+        let estratto = document.querySelector(".cartelle h2");
+        let numeri = document.querySelectorAll(".numero");
+        let cartelle = document.querySelectorAll(".cartella");
+        
+        estratto.innerText = `Ultimo numero estratto: ${numeroRandom}`;
+        
+        for (let n of numeri) {
+            if (n.innerText == numeroRandom) n.querySelector("div").classList.add("check");
         }
-    }
 
-    function estrazione (arr) {
-        do {
-            let nuovoNumero = Math.floor(Math.random() * 76) + 1;
-        } while (arr[arr.indexOf[nuovoNumero]] == 0);
-        arr[nuovoNumero] = 0;
-    
-        return nuovoNumero;
-    }
-});
+        for (let cartella of cartelle) {
+            let caselle = document.querySelectorAll(".div2 div");
+        
+            for (let casella of caselle) {
+                if (casella.innerText == numeroRandom) casella.classList.add("estratto");
+            }
+        }
+    });
+}
---
+++ m1/s3/progetto/D10.js
@@ -0,0 +1,502 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+// JS Basics
+
+/* ESERCIZIO A
+  Crea una variabile chiamata "sum" e assegnaci il risultato della somma tra i valori 10 e 20.
+*/
+let sum = 10 + 20;
+console.log(sum);
+
+/* ESERCIZIO B
+  Crea una variabile chiamata "random" e assegnaci un numero casuale tra 0 e 20 (deve essere generato dinamicamente a ogni esecuzione).
+*/
+let random = Math.floor(Math.random() * 21);
+console.log(random);
+
+/* ESERCIZIO C
+  Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
+*/
+let me = {
+  name: "Gabriele",
+  surname: "Iannuzzo",
+  age: 21,
+}
+console.log(me);
+
+/* ESERCIZIO D
+  Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
+*/
+delete me.age;
+console.log(me);
+
+/* ESERCIZIO E
+  Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
+*/
+me.skills = ["JavaScript", "C"];
+console.log(me);
+
+/* ESERCIZIO F
+  Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
+*/
+me.skills.push("Python");
+console.log(me);
+
+/* ESERCIZIO G
+  Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
+*/
+me.skills.pop();
+console.log(me);
+
+// Funzioni
+
+/* ESERCIZIO 1
+  Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
+*/
+function dice () {
+  let random = Math.floor(Math.random() * 6) + 1;
+  return random;
+}
+console.log(dice());
+
+/* ESERCIZIO 2
+  Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
+*/
+let whoIsBigger = (a, b) => {
+  return Math.max(a, b);
+}
+console.log(whoIsBigger(3, 8));
+
+/* ESERCIZIO 3
+  Crea una funzione chiamata "splitMe" che riceve una stringa come parametro e ritorna un'array contenente ogni parola della stringa.
+
+  Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
+*/
+function splitMe (str) {
+  let arr = str.split(" ");
+  return arr;
+}
+console.log(splitMe("fucsia Charles Leclerc"));
+
+/* ESERCIZIO 4
+  Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
+  Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
+*/
+let deleteOne = (str, bool) => {
+  if (bool) return str.substring(1);
+  return str.substring(0, str.length - 1);
+}
+console.log(deleteOne("ciao", false));
+
+/* ESERCIZIO 5
+  Crea una funzione chiamata "onlyLetters" che riceve una stringa come parametro e la ritorna eliminando tutte le cifre numeriche.
+
+  Es.: onlyLetters("I have 4 dogs") => ritorna "I have dogs"
+*/
+function onlyLetters (str) {
+  return str.replace(/[0-9]/g, '');
+}
+console.log(onlyLetters("fucsia1 Charles 22Leclerc8317939"));
+
+/* ESERCIZIO 6
+  Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
+*/
+let isThisAnEmail = str => {
+  let pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (pattern.test(str)) return true;
+  return false;
+}
+console.log(isThisAnEmail("gabriele@gmail.com"));
+
+/* ESERCIZIO 7
+  Scrivi una funzione chiamata "whatDayIsIt" che ritorna il giorno della settimana corrente.
+*/
+function whatDayIsIt () {
+  let today = new Date();
+  const options = {weekday: 'long'};
+  return today.toLocaleString('it-IT', options);
+}
+console.log(whatDayIsIt());
+
+/* ESERCIZIO 8
+  Scrivi una funzione chiamata "rollTheDices" che riceve un numero come parametro.
+  Deve invocare la precedente funzione dice() il numero di volte specificato nel parametro, e deve tornare un oggetto contenente una proprietà "sum":
+  il suo valore deve rappresentare il totale di tutti i valori estratti con le invocazioni di dice().
+  L'oggetto ritornato deve anche contenere una proprietà "values", contenente un array con tutti i valori estratti dalle invocazioni di dice().
+
+  Example:
+  rollTheDices(3) => ritorna {
+      sum: 10
+      values: [3, 3, 4]
+  }
+*/
+let rollTheDices = n => {
+  let object = {
+    sum: 0,
+    values: [],
+  };
+  let random;
+
+  for (let i = 0; i < n; i++) {
+    random = dice();
+    object.sum += random;
+    object.values.push(random);
+  }
+
+  return object;
+}
+console.log(rollTheDices(3));
+
+/* ESERCIZIO 9
+  Scrivi una funzione chiamata "howManyDays" che riceve una data come parametro e ritorna il numero di giorni trascorsi da tale data.
+*/
+function howManyDays (date) {
+  let day = new Date(date);
+  let today = new Date();
+  let diff = Math.floor((today.getTime() - day.getTime()) / (1000 * 3600 * 24));
+
+  return diff;
+}
+console.log(howManyDays("10-10-2023"));
+
+/* ESERCIZIO 10
+  Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
+*/
+function isTodayMyBirthday (date) {
+  let day = new Date(date);
+  let today = new Date();
+  if (today.getDay() === day.getDay() && today.getMonth() == day.getMonth()) return true;
+  return false;
+}
+console.log(isTodayMyBirthday("09-13-2023"));
+
+// Arrays & Oggetti
+
+// NOTA: l'array "movies" usato in alcuni esercizi è definito alla fine di questo file
+const movies = [
+  {
+    Title: 'The Lord of the Rings: The Fellowship of the Ring',
+    Year: '2001',
+    imdbID: 'tt0120737',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+  },
+
+  {
+    Title: 'The Lord of the Rings: The Return of the King',
+    Year: '2003',
+    imdbID: 'tt0167260',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Two Towers',
+    Year: '2002',
+    imdbID: 'tt0167261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of War',
+    Year: '2005',
+    imdbID: 'tt0399295',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lords of Dogtown',
+    Year: '2005',
+    imdbID: 'tt0355702',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings',
+    Year: '1978',
+    imdbID: 'tt0077869',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1990',
+    imdbID: 'tt0100054',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lords of Salem',
+    Year: '2012',
+    imdbID: 'tt1731697',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
+    Year: '1984',
+    imdbID: 'tt0087365',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1963',
+    imdbID: 'tt0057261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Avengers',
+    Year: '2012',
+    imdbID: 'tt0848228',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Infinity War',
+    Year: '2018',
+    imdbID: 'tt4154756',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Age of Ultron',
+    Year: '2015',
+    imdbID: 'tt2395427',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Endgame',
+    Year: '2019',
+    imdbID: 'tt4154796',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+  },
+]
+
+
+/* ESERCIZIO 11
+  Scrivi una funzione chiamata "deleteProp" che riceve un oggetto e una stringa come parametri; deve ritornare l'oggetto fornito dopo aver eliminato
+  in esso la proprietà chiamata come la stringa passata come secondo parametro.
+*/
+let deleteProp = (obj, str) => {
+  delete obj[str];
+  return obj;
+}
+console.log(deleteProp({nome: 'paolo', cognome: 'filiali', eta: 20}, 'cognome'));
+
+/* ESERCIZIO 12
+  Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
+*/
+function newestMovie () {
+  let years = [];
+  for (i of movies) years.push(i.Year);
+  let year = Math.max(...years);
+  let movie = movies.find(movie => movie.Year == year);
+  return movie;
+}
+console.log(newestMovie());
+
+/* ESERCIZIO 13
+  Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
+*/
+let countMovies = () => {
+  return movies.length;
+}
+console.log(countMovies());
+
+/* ESERCIZIO 14
+  Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
+*/
+function onlyTheYears () {
+  let years = [];
+  for (i of movies) years.push(i.Year);
+  return years;
+}
+console.log(onlyTheYears());
+
+/* ESERCIZIO 15
+  Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
+*/
+let onlyInLastMillennium = () => {
+  let lastMillennium = movies.filter(movie => movie.Year > 2000);
+  return lastMillennium;
+}
+console.log(onlyInLastMillennium());
+
+/* ESERCIZIO 16
+  Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
+*/
+function sumAllTheYears () {
+  let sum = movies.reduce((c, p) => {return c + parseInt(p.Year)}, 0);
+  return sum;
+}
+console.log(sumAllTheYears());
+
+/* ESERCIZIO 17
+  Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
+*/
+let searchByTitle = str => {
+  let arr = movies.filter(movie => movie.Title.includes(str));
+  return arr;
+}
+console.log(searchByTitle("Avengers"));
+
+/* ESERCIZIO 18
+  Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
+  "match" deve includere tutti i film dell'array "movies" fornito che contengono la stringa fornita all'interno del proprio titolo, mentre "unmatch" deve includere tutti i rimanenti.
+*/
+function searchAndDivide (str) {
+  let obj = {};
+
+  obj.match = movies.filter(movie => movie.Title.includes(str));
+  obj.unmacth = movies.filter(movie => !(movie.Title.includes(str)));
+
+  return obj;
+}
+console.log(searchAndDivide("Avengers"));
+
+/* ESERCIZIO 19
+  Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
+*/
+let removeIndex = n => {
+  movies.splice(n, 1);
+  return movies;
+}
+console.log(removeIndex(0));
+
+// DOM (nota: gli elementi che selezionerai non si trovano realmente nella pagina)
+
+/* ESERCIZIO 20
+  Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
+*/
+function cont () {
+  let container = document.getElementById("container");
+}
+
+/* ESERCIZIO 21
+  Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
+*/
+let everyTd = () => {
+  let tds = document.querySelectorAll("td");
+}
+
+/* ESERCIZIO 22
+  Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
+*/
+function tdsText () {
+  let tds = document.querySelectorAll("td");
+  for (td of tds) console.log(td.innerText);
+}
+
+/* ESERCIZIO 23
+  Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
+*/
+let addBgColor = () => {
+  let links = document.querySelectorAll("a");
+  for (a of links) a.style.backgroundColor = "red";
+}
+
+/* ESERCIZIO 24
+  Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
+*/
+function addNewLi () {
+  let myList = document.getElementById("myList");
+  let li = document.createElement("li");
+
+  myList.append(li);
+}
+
+/* ESERCIZIO 25
+  Scrivi una funzione per svuotare la lista non ordinata con id "myList".
+*/
+let empty = () => {
+  let myList = document.getElementById("myList");
+  myList.innerHTML = '';
+}
+
+/* ESERCIZIO 26
+  Scrivi una funzione per aggiungere ad ogni tag <tr> la classe CSS "test"
+*/
+function addTest () {
+  let trs = document.querySelectorAll("tr");
+  for (tr of trs) tr.classList.add("test");
+}
+
+// [EXTRA] JS Avanzato
+
+/* ESERCIZIO 27
+  Crea una funzione chiamata "halfTree" che riceve un numero come parametro e costruisce un mezzo albero di "*" (asterischi) dell'altezza fornita.
+
+  Esempio:
+  halfTree(3)
+
+  *
+  **
+  ***
+
+*/
+let halfTree = n => {
+  for (let i = 0; i < n; i++) {
+    console.log("*".repeat(i));
+  }
+}
+halfTree(5);
+
+/* ESERCIZIO 28
+  Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
+
+  Esempio:
+  tree(3)
+
+    *
+   ***
+  *****
+
+*/
+function tree (n) {
+  for (let i = 0; i < n; i++) {
+    let spaces = " ".repeat(n - i + 1);
+    let stars = "*".repeat(2 * i + 1);
+    console.log(spaces + stars);
+  }
+}
+tree(3);
+
+/* ESERCIZIO 29
+  Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
+*/
+let isItPrime = n => {
+  if (n == 1 || n == 0) return false;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+console.log(isItPrime(7));
