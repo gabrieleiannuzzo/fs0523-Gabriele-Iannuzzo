2668ac80b7f6a19235bde1f10bb18f97fa0668fc
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2023-11-11 13:42:08 +0100

    sesto commit

--- m2/s3/progetto/assets/js/dettagli.js
+++ m2/s3/progetto/assets/js/dettagli.js
@@ -1,3 +1,5 @@
+const apiKey = " Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRlMTEyZTMyNWM5NzAwMTg3ZjlmZTYiLCJpYXQiOjE2OTk2MTUwMjIsImV4cCI6MTcwMDgyNDYyMn0.R5RBuhRA3qqu2SpJbbKquMjlimyliws3H3IK9JwOFV0";
+const url = "https://striveschool-api.herokuapp.com/api/product/";
 const searchParams = new URLSearchParams(window.location.search);
 const productId = searchParams.get("productId");
 
@@ -7,51 +9,59 @@ const brand = document.getElementById("brand");
 const description = document.getElementById("description");
 const price = document.getElementById("price");
 
-fillPage();
+// fillPage();
 
-async function fillPage () {
-    const response = await fetch(url + productId, {
-        headers: {
-            "Authorization": `Bearer ${apiKey}`,
-        }
-    });
-    const product = await response.json();
+// async function fillPage () {
+//     const response = await fetch(url + productId, {
+//         headers: {
+//             "Authorization": `Bearer ${apiKey}`,
+//         }
+//     });
+//     const product = await response.json();
 
-    img.src = product.imageUrl;
-    title.innerText = product.name;
-    brand.innerText = product.brand;
-    description.innerText = product.description;
-    price.innerText = `${setPoints(product.price)}€`;
-}
+//     img.src = product.imageUrl;
+//     title.innerText = product.name;
+//     brand.innerText = product.brand;
+//     description.innerText = product.description;
+//     price.innerText = `${setPoints(product.price)}€`;
+// }
+
+let dataFetch = new Fetch (url + productId, apiKey);
+dataFetch.fetchData("GET");
 
 class Fetch {
-    constructor (url, apiKey, method, obj) {
+    constructor (url, apiKey) {
         this.url = url;
         this.apiKey = apiKey;
-        this.method = method;
     }
 
-    async productsFetch (action) {
-        if (this.method == "GET" || this.method == "DELETE") {
-            const response = await fetch (url, {
-                method: this.method,
-                headers: {
-                    "Authorization": this.apiKey,
-                }
-            });
-
-            const product = response.json();
-        } else {
-            let response = await fetch (url, {
-                method: this.method,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": this.apiKey,
-                },
-                body: JSON.stringify(obj),
-            });
+    async fetchData (method, obj) {
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": this.apiKey,
         }
 
-        action();
+        if (this.method == "GET" || this.method == "DELETE") {
+            const response = await fetch (url, {
+                method: method,
+                headers: headers,
+            });
+
+            const data = await response.json();
+        } else {
+            const response = await fetch (url, {
+                method: method,
+                headers: headers,
+                body: JSON.stringify(obj),
+            });
+
+            const data = await response.json()
+        }
+
+        return data;
+    }
+
+    handleDataFunction (data, handleData) {
+        handleData(data);
     }
 }
--- m2/s3/progetto/assets/js/funzioni.js
+++ m2/s3/progetto/assets/js/funzioni.js
@@ -1,6 +1,3 @@
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRlMTEyZTMyNWM5NzAwMTg3ZjlmZTYiLCJpYXQiOjE2OTk2MTUwMjIsImV4cCI6MTcwMDgyNDYyMn0.R5RBuhRA3qqu2SpJbbKquMjlimyliws3H3IK9JwOFV0";
-const url = "https://striveschool-api.herokuapp.com/api/product/";
-
 // FUNZIONE PER INSERIRE IL PUNTO DALLE MIGLIAIA IN POI
 function setPoints(number){
     if (number < 100) return number;
